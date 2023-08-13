9c5b67155d7a56a39146d9567904aa605052854c
Tirth <tirthacodes@gmail.com>
2023-08-13 10:06:56 +0545

    defined properties in User.ts entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/entities/User';
 
 @Module({
   imports: [
@@ -12,7 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'testuser',
       password: 'testuser123',
       database: 'nestjs_tutorial',
-      entities: [],
+      entities: [User],
       synchronize: true,
     })
   ],
