a7bcc0356cc471721a5882d6c99d610c3f95ede4
Tirth <tirthacodes@gmail.com>
2023-08-14 19:01:07 +0545

    injected userservice

--- src/users/controller/users/users.controller.ts
+++ src/users/controller/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { UsersService } from 'src/users/services/users/users.service';
 
 @Controller('users')
 export class UsersController {
+
+    constructor(
+        private userService: UsersService
+    ){}
+
     @Get()
     getUsers(){
 
@@ -9,7 +16,7 @@ export class UsersController {
     }
 
     @Post()
-    createUsers(){
+    createUsers(@Body() createUserDto: CreateUserDto){
         
     }
 }
